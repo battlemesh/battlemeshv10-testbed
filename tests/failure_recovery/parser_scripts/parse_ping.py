21a49da1840537514a75fa3fa0094d88d2907904
Leonardo Maccari <leonardo.maccari@unitn.it>
2017-06-09 17:07:31 +0200

    ping parser

---
+++ tests/failure_recovery/parser_scripts/parse_ping.py
@@ -0,0 +1,85 @@
+#!/bin/env python
+import glob
+from collections import defaultdict
+import numpy as np
+import sys
+
+
+data_path = sys.argv[1]+"*txt"
+
+file_list = glob.glob(data_path)
+
+data = defaultdict(dict)
+
+for f in file_list:
+    tokens = f.split("-")
+    s = tokens[1]
+    d = '.'.join(tokens[2].split(".")[:-1])
+    data[s][d] = {}
+    with open(f) as data_file:
+        content = data_file.readlines()
+        for entry in content:
+            if "loss" in entry:
+                data[s][d]['loss'] = int(entry.split()[-1][:-1])
+            if "max" in entry:
+                data[s][d]['max'] = float(entry.split("=")[1].split()[0])
+            if "min" in entry:
+                data[s][d]['min'] = float(entry.split("=")[1])
+            if "avg" in entry:
+                data[s][d]['avg'] = float(entry.split("=")[1])
+            if "entries" in entry:
+                data[s][d]['entries'] = [float(x)  for x in entry.split("=")[1].split()]
+
+print "source, dest, loss, min_rtt, median_rtt, max_rtt, 10_percent, 90_percent "
+for s in data:
+    for d, numbers in data[s].items():
+        print s, ",", d, ",",
+        if 'loss' in numbers:
+            print numbers['loss'], ",",
+        else:
+            print ",",
+        if 'min' in numbers:
+            print numbers['min'], ",",
+        else:
+            print ",",
+        if 'entries' in numbers:
+            print np.percentile(numbers['entries'], 50), ",",
+        else:
+            print ",",
+        #if 'avg' in numbers:
+        #    print numbers['avg'], ",",
+        #else:
+        #    print ",",
+        if 'max' in numbers:
+            print numbers['max'], ",",
+        else:
+            print ",",
+        if 'entries' in numbers:
+            print np.percentile(numbers['entries'], 10), ",",
+            print np.percentile(numbers['entries'], 90), ",",
+        else:
+            print ",,",
+        print ""
+
+
+print ""
+print ""
+ranked_data = {}
+max_length = 0
+for s in data:
+    for d, numbers in data[s].items():
+        ranked_data[s+d] = sorted(numbers['entries'], reverse=True)
+        if max_length < len(ranked_data[s+d]):
+            max_length = len(ranked_data[s+d])
+
+
+for i in range(max_length):
+    for k in ranked_data:
+        try:
+            print ranked_data[k][i], ",",
+        except IndexError:
+            print ",",
+    print ""
+
+
+
--- tests/failure_recovery/pingall.sh
+++ tests/failure_recovery/pingall.sh
@@ -1,11 +1,14 @@
 DEST=$1
-rm -f /tmp/failuretest*.txt
 DATAFILE="/tmp/failuretest-"`hostname`-"$DEST.txt"
+PINGOUT="/tmp/pingout-$DEST.txt"
 PING_NUM=100
-ping -q -n -c $PING_NUM  $DEST > /tmp/pingout.txt
-ENTRIES=$(grep "bytes from" /tmp/pingout.txt | awk '{printf $7}' |sed 's/time=/ /g')
-echo "entries = $ENTRIES" >> $DATAFILE
-cat /tmp/pingout.txt | grep "packet loss" | awk '{printf("loss = %s\n", $7)}' >> $DATAFILE
-cat /tmp/pingout.txt | grep "round-trip" | awk -F '/' '{printf($3)}' | awk -F "=" '{printf("min = %s\n",$2)}' >> $DATAFILE
-cat /tmp/pingout.txt | grep "round-trip" | awk -F '/' '{printf("avg = %s\n", $4)}' >> $DATAFILE
-cat /tmp/pingout.txt | grep "round-trip" | awk -F '/' '{printf("max = %s\n", $5)}' >> $DATAFILE
+ping -n -c $PING_NUM  $DEST > $PINGOUT
+ENTRIES=$(grep "bytes from" $PINGOUT | awk '{printf $7}' |sed 's/time=/ /g')
+echo "entries = $ENTRIES" > $DATAFILE
+cat $PINGOUT | grep "packet loss" | awk '{printf("loss = %s\n", $7)}' >> $DATAFILE
+cat $PINGOUT | grep "round-trip" | awk -F '/' '{printf($3)}' | awk -F "=" '{printf("min = %s\n",$2)}' >> $DATAFILE
+cat $PINGOUT | grep "round-trip" | awk -F '/' '{printf("avg = %s\n", $4)}' >> $DATAFILE
+cat $PINGOUT | grep "round-trip" | awk -F '/' '{printf("max = %s\n", $5)}' >> $DATAFILE
+
+#IPERFOUT="/tmp/failuretest-iperf-"`hostname`"-$DEST.csv"
+#iperf -f M -y C -t 10 -e -V -c $DEST | tail -n 2 > $IPERFOUT
