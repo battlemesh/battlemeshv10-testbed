1c6178f0828dcd6fbf8a624bdeb1ec2c2ea81203
Antonio Quartulli <a@unstable.cc>
2017-06-09 19:03:46 +0800

    tests: get latency from ping results

--- tests/failure_recovery/pingall.sh
+++ tests/failure_recovery/pingall.sh
@@ -3,6 +3,8 @@ rm -f /tmp/failuretest*.txt
 DATAFILE="/tmp/failuretest-"`hostname`-"$DEST.txt"
 PING_NUM=20
 ping -q -n -c $PING_NUM  $DEST > /tmp/pingout.txt
+ENTRIES=$(grep "bytes from" /tmp/pingout.txt | awk '{printf $7}' |sed 's/time=/ /g')
+echo "entries = $ENTRIES" >> $DATAFILE
 cat /tmp/pingout.txt | grep "packet loss" | awk '{printf("loss = %s\n", $7)}' >> $DATAFILE
 cat /tmp/pingout.txt | grep "round-trip" | awk -F '/' '{printf($3)}' | awk -F "=" '{printf("min = %s\n",$2)}' >> $DATAFILE
 cat /tmp/pingout.txt | grep "round-trip" | awk -F '/' '{printf("avg = %s\n", $4)}' >> $DATAFILE
