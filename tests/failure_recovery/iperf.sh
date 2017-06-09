c7a815491fa6e0f84ec11fcd661329039a84aed6
Leonardo Maccari <leonardo.maccari@unitn.it>
2017-06-09 18:06:51 +0200

    iperf data

--- tests/failure_recovery/iperf.sh
+++ tests/failure_recovery/iperf.sh
@@ -1,4 +1,4 @@
 DEST=$1
 
 IPERFOUT="/tmp/failuretest-iperf-"`hostname`"-$DEST.csv"
-iperf -f M -y C -t 10 -e -V -c $DEST | tail -n 2 > $IPERFOUT
+iperf -f M -y C -t 20 -e -V -c $DEST | tail -n 2 > $IPERFOUT
